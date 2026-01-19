b0e8ffe8eda83da5b07c49a60aee61be436d5800
Mansour Abdelkader <mansourabdelkader90@gmail.com>
2026-01-19 10:38:55 +0100

    fix pi button

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
+"use client";
 import Image from "next/image";
+import { PiButton } from "@/components/PiButton";
 
 export default function Home() {
   return (
@@ -34,6 +36,7 @@ export default function Home() {
             center.
           </p>
         </div>
+<PiButton />
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <a
             className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
